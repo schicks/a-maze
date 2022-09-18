76739baf37bb668bd04e8ef52993906f222dd174
sam schick <schicksw@protonmail.com>
2022-09-18 15:47:09 -0400

    removed GameInput type, made consts associated

--- src/input.rs
+++ src/input.rs
@@ -4,16 +4,6 @@ use bevy::prelude::*;
 use bytemuck::{Pod, Zeroable};
 use ggrs::PlayerHandle;
 
-#[derive(PartialEq, Debug)]
-pub enum GameInput {
-    UpRight,
-    Up,
-    UpLeft,
-    DownLeft,
-    Down,
-    DownRight,
-    Interact,
-}
 /*
 * GGRS needs to represent input as "just data" (particularly, it need to implement bytemuck::pod).
 * We want to be working with binary representations as little as possible,
@@ -25,36 +15,31 @@ pub enum GameInput {
 #[derive(Copy, Clone, PartialEq, Eq, Pod, Zeroable)]
 pub struct InputSet(u8);
 
-// These are isolated input representations. It's important that they only have one positive bit.
-// 00000000
-const INPUT_EMPTY: InputSet = InputSet(0);
-// 00000001
-const INPUT_UP_RIGHT: InputSet = InputSet(1 << 0);
-// 00000010
-const INPUT_UP: InputSet = InputSet(1 << 1);
-// 00000100
-const INPUT_UP_LEFT: InputSet = InputSet(1 << 2);
-// 00001000
-const INPUT_DOWN_LEFT: InputSet = InputSet(1 << 3);
-// 00010000
-const INPUT_DOWN: InputSet = InputSet(1 << 4);
-// 00100000
-const INPUT_DOWN_RIGHT: InputSet = InputSet(1 << 5);
-// 01000000
-const INPUT_INTERACT: InputSet = InputSet(1 << 6);
+impl InputSet {
+    // These are isolated input representations. It's important that they only have one positive bit.
+    // This also means that we are limited to 8 inputs for a u8, 16 for u16, etc.
+    // 00000000
+    const EMPTY: InputSet = InputSet(0);
+    // 00000001
+    const UP_RIGHT: InputSet = InputSet(1 << 0);
+    // 00000010
+    const UP: InputSet = InputSet(1 << 1);
+    // 00000100
+    const UP_LEFT: InputSet = InputSet(1 << 2);
+    // 00001000
+    const DOWN_LEFT: InputSet = InputSet(1 << 3);
+    // 00010000
+    const DOWN: InputSet = InputSet(1 << 4);
+    // 00100000
+    const DOWN_RIGHT: InputSet = InputSet(1 << 5);
+    // 01000000
+    const INTERACT: InputSet = InputSet(1 << 6);
 
-impl From<GameInput> for InputSet {
-    // this lets us get isolated representations easily, without exposing the constants outside this module.
-    fn from(input: GameInput) -> InputSet {
-        match input {
-            GameInput::UpRight => INPUT_UP_RIGHT,
-            GameInput::Up => INPUT_UP,
-            GameInput::UpLeft => INPUT_UP_LEFT,
-            GameInput::DownLeft => INPUT_DOWN_LEFT,
-            GameInput::Down => INPUT_DOWN,
-            GameInput::DownRight => INPUT_DOWN_RIGHT,
-            GameInput::Interact => INPUT_INTERACT,
-        }
+    pub fn contains(self, input: InputSet) -> bool {
+        /*
+         * Checks whether `self` contains all the elements of `input`.
+         */
+        self & input == input
     }
 }
 
@@ -76,27 +61,19 @@ impl BitAnd for InputSet {
     }
 }
 
-impl InputSet {
-    pub fn contains(self, input: GameInput) -> bool {
-        /*
-        * Checks whether set contains the given input.
-        */
-        self & InputSet::from(input) != INPUT_EMPTY
-    }
-}
-
+// mapping between isolated representations and the keys for creating them.
 const KEYMAP: [(KeyCode, InputSet); 7] = [
-    (KeyCode::Q, INPUT_UP_LEFT),
-    (KeyCode::W, INPUT_UP),
-    (KeyCode::E, INPUT_UP_RIGHT),
-    (KeyCode::A, INPUT_DOWN_LEFT),
-    (KeyCode::S, INPUT_DOWN),
-    (KeyCode::D, INPUT_DOWN_RIGHT),
-    (KeyCode::Return, INPUT_INTERACT),
+    (KeyCode::Q, InputSet::UP_LEFT),
+    (KeyCode::W, InputSet::UP),
+    (KeyCode::E, InputSet::UP_RIGHT),
+    (KeyCode::A, InputSet::DOWN_LEFT),
+    (KeyCode::S, InputSet::DOWN),
+    (KeyCode::D, InputSet::DOWN_RIGHT),
+    (KeyCode::Return, InputSet::INTERACT),
 ];
 
-fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> Vec<InputSet> {
-    let folded = KEYMAP.iter().fold(INPUT_EMPTY, |acc, a| {
+pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> Vec<InputSet> {
+    let folded = KEYMAP.iter().fold(InputSet::EMPTY, |acc, a| {
         let (key, i) = a;
         if keys.pressed(*key) {
             acc | *i
